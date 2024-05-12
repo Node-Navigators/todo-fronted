1d0c20093ff7d3014042e6bcbcc0d91d62d7b7ee
yaseeen96 <syedyaseen1539@gmail.com>
2024-05-12 19:36:58 +0530

    create base repository

---
+++ README.md
@@ -0,0 +1,16 @@
+# TODO Project
+
+### Guidelines to run the project
+
+1. Navigate to directory
+2. Run this command
+
+```
+npm run dev
+```
+
+3. Build project
+
+```
+npm run build
+```
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-900">
+        TODO WEB APP FRONTEND
+      </h1>
+    </div>
+  );
+}
+
+export default App;
