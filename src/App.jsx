d9d6ffc63c75adc48926adce56afa8d7191ae69f
yaseeen96 <syedyaseen1539@gmail.com>
2024-05-14 11:37:04 +0530

    add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
   HomePage,
@@ -7,18 +6,21 @@ import {
   ForgotPasswordPage,
   PageNotFound,
 } from "./pages";
+import { DarkModeProvider } from "./utils/check_dark_mode";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <DarkModeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </DarkModeProvider>
   );
 }
 
--- src/pages/login_page.jsx
+++ src/pages/login_page.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDarkMode } from "../utils/check_dark_mode";
 
 const LoginPage = () => {
+  let isDarkMode = useDarkMode();
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="w-screen h-screen flex items-center bg-white dark:bg-black dark:text-red-50 text-black">
+      <div className="w-1/2 h-full">
+        {console.log(isDarkMode)}
+        {isDarkMode ? (
+          <img src="src/assets/logo-white.png" alt="" className="w-full" />
+        ) : (
+          <img src="src/assets/logo-black.png" alt="" className="w-full" />
+        )}
+      </div>
     </div>
   );
 };
